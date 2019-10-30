e37fb6b7438b713a20d41f48d61ad7816c13d4b2
DevEAL <desarrollo@enalgunlugarestudio.com>
2019-10-30 10:06:47 -0500

    Api

---
+++ Api/public/index.php
@@ -0,0 +1,23 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/config/db.php';
+require __DIR__ . '/../src/utils/printJson.php';
+require __DIR__ . '/../src/utils/sedMail.php';
+require __DIR__ . '/../src/utils/CrearHTML.php';
+
+require __DIR__ . '/../src/controller/Inscription.php';
+require __DIR__ . '/../src/model/Inscription.php';
+
+require __DIR__ . '/../src/controller/Contact.php';
+require __DIR__ . '/../src/model/Contact.php';
+
+$app = new \Slim\App;
+
+
+// Routes
+require __DIR__. '/../src/routes/Incription.php';
+
+$app->run();
